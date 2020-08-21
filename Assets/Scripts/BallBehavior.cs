70998ae3f8fc3f0db9259ff583c664c9845c85ba
MargaritaLomova <57067240+MargaritaLomova@users.noreply.github.com>
2020-08-21 20:42:59 +0300

    first commit

---
+++ Assets/Scripts/BallBehavior.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class BallBehavior : MonoBehaviour
+{
+    /// <summary>
+    /// Rigidbody сферы
+    /// </summary>
+    private Rigidbody rigidbody;
+    /// <summary>
+    /// Скорость перемещения
+    /// </summary>
+    [SerializeField] private float ballSpeed = 1f;
+    /// <summary>
+    /// Стартовое положение (для мыши)
+    /// </summary>
+    private Vector3 startPos;
+    private void Start()
+    {
+        //Присваиваем rigidbody
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+#if UNITY_ANDROID || UNITY_IOS
+        //Если есть касание
+        if(Input.touchCount > 0)
+        {
+            //Сохраняем в переменную
+            var touch = Input.GetTouch(0);
+            switch (touch.phase)
+            {
+                //Если касание в движении
+                case TouchPhase.Moved:
+                    //Снимаем ограничение на вращение
+                    rigidbody.constraints = RigidbodyConstraints.None;
+                    //Вычисляем смещение
+                    offset =  new Vector3(touch.deltaPosition.x, transform.position.y, touch.deltaPosition.y);
+                    //Перемещаем в зависимости от направления движения тача
+                    rigidbody.AddForce(offset * (ballSpeed / 900));
+                    break;
+                //Если касание закончилось
+                case TouchPhase.Ended:
+                    //Замораживаем вращение
+                    rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+                    break;
+            }
+        }
+#endif
+    }
+#if UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX
+    //Когда пользователь зажал кнопку мыши
+    private void OnMouseDown()
+    {
+        //Сохраняем стратовое положение
+        startPos = Input.mousePosition;
+    }
+    //Когда пользователь двигает мышью
+    private void OnMouseDrag()
+    {
+        //Снимаем ограничение на вращение
+        rigidbody.constraints = RigidbodyConstraints.None;
+        //Вычисляем смещение
+        var offset = new Vector3(Input.mousePosition.x - startPos.x, transform.position.y, Input.mousePosition.y - startPos.y);
+        //Двигаем сферу
+        rigidbody.AddForce(offset * (ballSpeed / 900));
+    }
+    //Когда пользователь отпустил кнопку мыши
+    private void OnMouseUp()
+    {
+        //Замораживаем вращение
+        rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+#endif
+}
---
+++ Assets/Scripts/FigureBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FigureBehavior : MonoBehaviour
+{
+    /// <summary>
+    /// Список всех частей фигуры
+    /// </summary>
+    private List<GameObject> parts = new List<GameObject>();
+    /// <summary>
+    /// Общий коллайдер фигуры
+    /// </summary>
+    private BoxCollider collider;
+    private void Start()
+    {
+        //Присваиваем значение коллайдеру
+        collider = GetComponent<BoxCollider>();
+        //Для каждого дочернего объекта фигуры
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            //Добавляем в список
+            parts.Add(transform.GetChild(i).gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        //Если сфера столкнулась с фигурой
+        if(collision.gameObject.tag == "Player")
+        {
+            //Уничтожаем общий коллайдер
+            Destroy(collider);
+            //Для каждой части фигуры
+            foreach(var part in parts)
+            {
+                //Делаем часть динамичной
+                part.GetComponent<Rigidbody>().isKinematic = false;
+            }
+        }
+    }
+}
